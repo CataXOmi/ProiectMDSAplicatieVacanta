3cad2cb197b47ca5b30ec272bb3a693aa8f68027
CONSTANTIN CATALIN MARIN <constantin.marin@s.unibuc.ro>
2021-03-24 22:04:55 +0200

    first controller

---
+++ ProiectMDS/Controllers/AtractieController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProiectMDS.Models;
+using ProiectMDS.DTOs;
+using ProiectMDS.Repositories.AtractieRepository;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProiectMDS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AtractieController : ControllerBase
+    {
+        public IAtractieRepository IAtractieRepository { get; set; }
+        public AtractieController(IAtractieRepository repository)
+        {
+            IAtractieRepository = repository;
+        }
+
+        // GET: api/<AtractieController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Atractie>> Get()
+        {
+            return IAtractieRepository.GetAll();
+        }
+
+        // GET api/<AtractieController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Atractie> Get(int id)
+        {
+            return IAtractieRepository.Get(id);
+        }
+
+        // POST api/<AtractieController>
+        [HttpPost]
+        public Atractie Post(AtractieDTO value)
+        {
+            Atractie model = new Atractie()
+            {
+                Denumire = value.Denumire,
+                OraDeschidere = value.OraDeschidere,
+                OraInchidere = value.OraInchidere,
+                Pret = value.Pret,
+                Oras = value.Oras, 
+                Adresa = value.Adresa
+            };
+            return IAtractieRepository.Create(model);
+        }
+
+        // PUT api/<AtractieController>/5
+        [HttpPut("{id}")]
+        public Atractie Put(int id, AtractieDTO value)
+        {
+            Atractie model = IAtractieRepository.Get(id);
+
+            if (value.Denumire != null)
+            {
+                model.Denumire = value.Denumire;
+            }
+
+            if (value.OraDeschidere != TimeSpan.Zero)
+            {
+                model.OraDeschidere = value.OraDeschidere;
+            }
+
+            if (value.OraInchidere != TimeSpan.Zero)
+            {
+                model.OraInchidere = value.OraInchidere;
+            }
+
+            if (value.Pret != 0)
+            {
+                model.Pret = value.Pret;
+            }
+
+            if (value.Oras != null)
+            {
+                model.Oras = value.Oras;
+            }
+
+            if (value.Adresa != null)
+            {
+                model.Adresa = value.Adresa;
+            }
+
+            return IAtractieRepository.Update(model);
+        }
+
+        // DELETE api/<AtractieController>/5
+        [HttpDelete("{id}")]
+        public Atractie Delete(int id)
+        {
+            Atractie atractie = IAtractieRepository.Get(id);
+            return IAtractieRepository.Delete(atractie);
+        }
+    }
+}
---
+++ ProiectMDS/Controllers/RezervareCazareController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProiectMDS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RezervareCazareController : ControllerBase
+    {
+        // GET: api/<RezervareCazareController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<RezervareCazareController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RezervareCazareController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<RezervareCazareController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RezervareCazareController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
